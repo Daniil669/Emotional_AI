482490904ad64b56eb9d690e70ae71d0b247f41d
Daniil669 <daniil.freeman669@gmail.com>
2025-08-28 12:46:41 +0300

    delet test_wav.py

--- ai/scripts/test_tone_wav.py
+++
@@ -1,9 +0,0 @@
-import wave, math, struct
-sr, dur, freq = 16000, 1.0, 440.0
-n = int(sr*dur)
-with wave.open("test.wav","w") as w:
-    w.setnchannels(1); w.setsampwidth(2); w.setframerate(sr)
-    for i in range(n):
-        val = int(0.2*32767*math.sin(2*math.pi*freq*i/sr))
-        w.writeframes(struct.pack('<h', val))
-print("Wrote test.wav")
